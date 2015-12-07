e32effc87c8f10fadadb135f2d00e5a6a0ea73b2
Toma Becea <tbecea@macadamian.com>
2015-12-07 17:17:07 +0200

    ChatterBox: proper closing of audio and video tracks on hanging up.

--- ChatterBox.Client.Voip/States/HangingUp.cs
+++ ChatterBox.Client.Voip/States/HangingUp.cs
@@ -4,6 +4,8 @@ using ChatterBox.Client.Common.Communication.Voip.States;
 using ChatterBox.Client.Voip.States.Interfaces;
 using ChatterBox.Common.Communication.Messages.Relay;
 using Microsoft.Practices.Unity;
+using System.Collections.Generic;
+using webrtc_winrt_api;
 
 namespace ChatterBox.Client.Common.Communication.Voip.States
 {
@@ -27,13 +29,36 @@ namespace ChatterBox.Client.Common.Communication.Voip.States
                 Context.PeerId = null;
             }
 
+            StopVideoTracks(Context.LocalStream?.GetVideoTracks());
+            StopAudioTracks(Context.LocalStream?.GetAudioTracks());
             Context.LocalStream?.Stop();
+
+            StopVideoTracks(Context.RemoteStream?.GetVideoTracks());
+            StopAudioTracks(Context.RemoteStream?.GetAudioTracks());
             Context.RemoteStream?.Stop();
 
             Context.VoipCoordinator.OnEnterHangingUp();
 
             var idleState = new VoipState_Idle();
             Context.SwitchState(idleState);
+        }
+
+        private void StopVideoTracks(IList<MediaVideoTrack> tracks)
+        {
+            if (tracks == null) return;
+            foreach (var track in tracks)
+            {
+                track.Stop();
+            }
+        }
+
+        private void StopAudioTracks(IList<MediaAudioTrack> tracks)
+        {
+            if (tracks == null) return;
+            foreach (var track in tracks)
+            {
+                track.Stop();
+            }
         }
     }
 }
